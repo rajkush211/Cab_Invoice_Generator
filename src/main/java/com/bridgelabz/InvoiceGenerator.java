17f036c4d87555382247ab231d1dcd86fd9d442e
rajkush211 <rajkush211.rk@gmail.com>
2020-04-20 08:40:01 +0530

    ADDED : Code to return minimum fare when total fare is less than minimum fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -3,8 +3,10 @@ package com.bridgelabz;
 public class InvoiceGenerator {
     private static final int COST_PER_MINUTE = 1;
     private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance, int time) {
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        double totalfare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_MINUTE;
+        return (totalfare < MINIMUM_FARE) ? MINIMUM_FARE : totalfare;
     }
 }
--- src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -12,4 +12,13 @@ public class CabInvoiceGeneratorTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
+
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare, 0.0);
+    }
 }
