a35e330981834b346e7ee25b2f835ae83db8fa6c
rajkush211 <rajkush211.rk@gmail.com>
2020-04-20 09:43:30 +0530

    ADDED : Code to return list of Invoice service from ride repository when given user id

--- src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceServiceTest.java
@@ -2,23 +2,22 @@ package com.bridgelabz;
 
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CabInvoiceGeneratorTest {
+public class CabInvoiceServiceTest {
 
-    private InvoiceGenerator invoiceGenerator = null;
+    private InvoiceService invoiceService = null;
 
     @Before
     public void setUp() throws Exception {
-        invoiceGenerator = new InvoiceGenerator();
+        invoiceService = new InvoiceService();
     }
 
     @Test
     public void whenGivenDistanceAndTime_ShouldReturnTotalFare() {
         double distance = 2.0;
         int time = 5;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 0.0);
     }
 
@@ -26,7 +25,7 @@ public class CabInvoiceGeneratorTest {
     public void givenLessDistanceOrTime_ShouldReturnMinimumFare() {
         double distance = 0.1;
         int time = 1;
-        double fare = invoiceGenerator.calculateFare(distance, time);
+        double fare = invoiceService.calculateFare(distance, time);
         Assert.assertEquals(5, fare, 0.0);
     }
 
@@ -36,7 +35,20 @@ public class CabInvoiceGeneratorTest {
                 new Ride(2.0, 5),
                 new Ride(0.1, 1)
         };
-        InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary summary = invoiceService.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
+    }
+
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoice() {
+        String userId = "abc.com";
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        invoiceService.addRides(userId, rides);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(userId);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
         Assert.assertEquals(expectedInvoiceSummary, summary);
     }
